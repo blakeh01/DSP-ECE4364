4085c0aefedeef520444d79b1f06009f08253592
Blake Havens <bmhavens6@gmail.com>
2024-01-17 03:35:11 -0600

    Update hwk_1.m
    Fixed pause between notes and added jingle bells.

--- hwk1/hwk_1.m
+++ hwk1/hwk_1.m
@@ -1,5 +1,6 @@
 clc
 clear
+close all
 
 %% Establish Parameters
 Fs = 8000;     % samp rate of 8 kHz
@@ -75,34 +76,99 @@ ttls_noteCell = {
     createNote(C, 2, vol, bpm, Fs)
 };
 
-waveform = createMusic(ttls_noteCell, 60/bpm, Fs); % rest is 1 beat
+jb_noteCell = {
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 2, vol, bpm, Fs),
+
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 2, vol, bpm, Fs),
+
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(G, 1, vol, bpm, Fs),
+    createNote(C, 1.5, vol, bpm, Fs),
+    createNote(D, 0.5, vol, bpm, Fs),
+
+    createNote(E, 4, vol, bpm, Fs),
+
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(F, 1.5, vol, bpm, Fs),
+    createNote(F, 0.5, vol, bpm, Fs),
+
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 0.5, vol, bpm, Fs),
+    createNote(E, 0.5, vol, bpm, Fs),
+
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(D, 1, vol, bpm, Fs),
+    createNote(D, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+
+    createNote(D, 2, vol, bpm, Fs),
+    createNote(G, 2, vol, bpm, Fs),
+
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 2, vol, bpm, Fs),
+
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 2, vol, bpm, Fs),
+
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(G, 1, vol, bpm, Fs),
+    createNote(C, 1.5, vol, bpm, Fs),
+    createNote(D, 0.5, vol, bpm, Fs),
+
+    createNote(E, 4, vol, bpm, Fs),
+
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(F, 1, vol, bpm, Fs),
+
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 1, vol, bpm, Fs),
+    createNote(E, 0.5, vol, bpm, Fs),
+    createNote(E, 0.5, vol, bpm, Fs),
+
+    createNote(G, 1, vol, bpm, Fs),
+    createNote(G, 1, vol, bpm, Fs),
+    createNote(F, 1, vol, bpm, Fs),
+    createNote(D, 1, vol, bpm, Fs),
+
+    createNote(C, 4, vol, bpm, Fs),
+};
+
+waveform = createMusic(jb_noteCell);
 
 %% Play and save music
 
 sound(waveform, Fs);
 audiowrite('output.wav', waveform, Fs);
+plot(waveform)
 
 %% Music functions
 
-% given an input of sinusoidals, concatenate them with
-% a pause between each note
-function songArr = createMusic(noteCell, pauseDuration, Fs)
+% given an input of sinusoidals, concatenate them
+function songArr = createMusic(noteCell)
     songArr = [];
     
     for i = 1:size(noteCell)
         songArr = [songArr, noteCell{i}];  % concat note
-
-        pauseSamps = zeros(1, round(Fs * pauseDuration)); % create pause, rounding to allow non-integer pauses
-        songArr = [songArr, pauseSamps];    % concat pause
     end
 
 end
 
-% create a function to create a sinusoidal of known
-% freq, amp, and duration.
+% A function to create a sinusoidal of known freq, amp, and duration.
 function note = createNote(freq, beats, vol, bpm, Fs)
-    % convert parameter 'beats' into seconds sampled @ Fs
+    % convert parameter 'beats' into seconds, sampled @ Fs
     duration = (60/bpm) * beats;
     t = 0:(1/Fs):(duration);
-    note = vol * cos(2*pi*freq*t) .* exp(-5*t / duration); % small exp. roll off to help with 'sharpness' of the sound.
+    note = vol * cos(2*pi*freq*t) .* exp(-5*t / duration); % small exp. roll off to help with 'sharpness' of the sound and create a pause between each beat.
 end
